095c7186b1e60ddd3bb22f473a1b51c1a23b3e49
AtharvaKedar <85386407+AtharvaKedar@users.noreply.github.com>
2021-06-05 15:39:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>BWD in JavaScript and React JS </h1>
+      <p>
+        7 Days, Certified, Free Web development with Javascript & React.Js Here
+        we bring our another free live BOOTCAMP with a hands on project at the
+        end; Web Development With Javascript & React.js The Foundation of Web
+        Development 🖥 Prerequisite : None Duration- 7 days Topics to be covered-
+        INTRO TO WEB DEVELOPMENT •INTRO TO PROGRAMMING IN JAVASCRIPT & REACT.JS
+        • FOUNDATIONAL KNOWLEDGE IN WEB DEVELOPMENT (JAVASCRIPT & REACT JS) •
+        HANDS ON PROJECT And much more ... So what are you waiting for? Learn
+        from upcoming intern at JIO, Microsoft and Goldman Sachs. And get
+        CERTIFIED✨ at the end.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
